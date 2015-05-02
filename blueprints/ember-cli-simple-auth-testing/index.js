ac6c82d2029ca5921a13e9459073bbdd3a16dcf0
Roger Adams <adamsrog04@gmail.com>
2015-05-02 13:26:12 -0700

    automatically import the helpers into start-app.js

--- blueprints/ember-cli-simple-auth-testing/index.js
+++ blueprints/ember-cli-simple-auth-testing/index.js
@@ -12,6 +12,12 @@ module.exports = {
       { after: '  "predef": [' + EOL }
     );
 
+    this.insertIntoFile('tests/helpers/start-app.js',
+      "import initializeTestHelpers from 'simple-auth-testing/test-helpers';" + EOL +
+      "initializeTestHelpers();" + EOL,
+      { after: "import config from '../../config/environment';" + EOL }
+    );
+
     return this.addBowerPackageToProject('ember-simple-auth', '0.8.0-beta.2');
   }
 };
